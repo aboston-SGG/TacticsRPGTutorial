dbaf5f42d09f1d926f21beaf9c40d4443aba8a7d
aboston-SGG <abostonmain@gmail.com>
2025-09-06 05:01:31 -0400

    Update Tile.cs

--- Episode 2/Tile.cs
+++ Episode 2/Tile.cs
@@ -1,70 +1,7 @@
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class Tile : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+public class Tile : MonoBehaviour
 {
     public Vector2Int gridPosition;
-
-    [Header("Colors")]
-    public Color originalColor;
-    public Color highlightColor = Color.yellow;
-    public Color selectedColor = Color.blue;
-
-    private Renderer tileRenderer;
-    public static Tile selectedTile;
-    public bool inMoveRange = false;
-
-    private void Start()
-    {
-        tileRenderer = GetComponentInChildren<Renderer>();
-    }
-
-    public void ChangeColor(Color newColor)
-    {
-        tileRenderer.material.color = newColor;
-    }
-
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        if (selectedTile != this)
-        {
-            ChangeColor(highlightColor);
-        }
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        if (selectedTile != this)
-        {
-            ChangeColor(inMoveRange? Color.cyan : originalColor);
-        }
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        SelectTile();
-
-        if (Player.selectedUnit)
-        {
-            if (inMoveRange)
-            {
-                Player.selectedUnit.MoveTo(transform.position, gridPosition);
-            }
-            else
-            {
-                Player.selectedUnit = null;
-            }
-        }
-    }
-
-    private void SelectTile()
-    {
-        if (selectedTile)
-        {
-            selectedTile.ChangeColor(selectedTile.originalColor);
-        }
-
-        selectedTile = this;
-        ChangeColor(selectedColor);
-    }
 }
+
